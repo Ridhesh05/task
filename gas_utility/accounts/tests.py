b12d43a36ea79c4318d5eb8b25f14c476d1f7216
Ridhesh05 <ridheshchauhan5@gmail.com>
2025-05-14 16:41:39 +0530

    Unit test of Auth in Accounts

--- gas_utility/accounts/tests.py
+++ gas_utility/accounts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework import status
 
-# Create your tests here.
+class AuthTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+    def test_login_success(self):
+        url = '/api/token/'
+        data = {'username': 'testuser', 'password': 'testpass'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+
+    def test_login_failure(self):
+        url = '/api/token/'
+        data = {'username': 'wronguser', 'password': 'wrongpass'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
